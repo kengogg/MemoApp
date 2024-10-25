e808525123a8dcc0cee37879d1f230ee7704c7ea
kengo <Shimazakikengo00@icloud.com>
2024-10-25 18:51:14 +0900

    Install ESLint

--- Desktop/udemy/react-native/my-app/App.tsx
+++ Desktop/udemy/react-native/my-app/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = (): JSX.Element => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!!!</Text>
@@ -17,4 +17,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
+
+export default App
