33a9b7683b7233cf77cb00949cf6c6c2bbada11b
kengo <Shimazakikengo00@icloud.com>
2024-10-27 14:54:18 +0900

    Markuo memo list screen

--- Desktop/udemy/react-native/my-app/src/app/index.tsx
+++ Desktop/udemy/react-native/my-app/src/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 // export default function Page() {
 //   return (
@@ -13,13 +13,71 @@ import { Text, View } from "react-native";
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>
-        Hello World
-      </Text>
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウt</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月28日 10:00</Text>
+          </View>
+          <View>
+            <Text>
+              X
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月28日 10:00</Text>
+          </View>
+          <View>
+            <Text>
+              X
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月28日 10:00</Text>
+          </View>
+          <View>
+            <Text>
+              X
+            </Text>
+          </View>
+        </View>
+
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
+
     </View>
   )
 }
 
-// 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
 export default Index
