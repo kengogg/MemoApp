475905f5c42ca5931ea6f5660b850820c6c6c8a9
kengo <Shimazakikengo00@icloud.com>
2024-10-27 14:31:55 +0900

    Install expo-router

---
+++ Desktop/udemy/react-native/my-app/src/app/index.tsx
@@ -0,0 +1,25 @@
+import { Text, View } from "react-native";
+
+// export default function Page() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.main}>
+//         <Text style={styles.title}>Hello World</Text>
+//         <Text style={styles.subtitle}>This is the first page of your app.</Text>
+//       </View>
+//     </View>
+//   );
+// }
+
+const Index = (): JSX.Element => {
+  return (
+    <View>
+      <Text>
+        Hello World
+      </Text>
+    </View>
+  )
+}
+
+// 
+export default Index
